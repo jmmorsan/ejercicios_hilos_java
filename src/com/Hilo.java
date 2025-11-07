e85afaa09d894718c83ec4f8ff1ccbc1b481d1b6
jmmorsan <moreno.sanchez.juan.manuel@iescamas.es>
2025-11-07 20:03:27 +0100

    Hilo en ejecución

--- src/com/Hilo.java
+++ src/com/Hilo.java
@@ -9,8 +9,7 @@ public class Hilo implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("🟢 Iniciando hilo: " + nombre +
-                           " en " + Thread.currentThread().getName());
+        System.out.println("🟢 Iniciando hilo: " + nombre + " en " + Thread.currentThread().getName());
         System.out.println("🔴 Finaliza hilo: " + nombre);
     }
 }
--- src/com/LanzadorHilosNew.java
+++ src/com/LanzadorHilosNew.java
@@ -1,8 +1,10 @@
 package com;
 
 public class LanzadorHilosNew {
-    public static void main(String[] args) {
-        Thread h = new Thread(new Hilo("nuevo"));
-        System.out.println(h.getName() + " -> " + h.getState());
+    public static void main(String[] args) throws InterruptedException {
+        Thread h = new Thread(new Hilo("runneable"));
+        h.start();
+        System.out.println(h.getName() + " ---> " + h.getState());
+        Thread.sleep(50); 
     }
 }
